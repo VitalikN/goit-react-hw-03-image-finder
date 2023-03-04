4c3283364faafb15a04226df8a8d9ddbe938e3b9
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2023-03-04 20:57:39 +0200

    modal-root

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -6,23 +6,13 @@ export const Box = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.6);
-`;
-export const ModalContent = styled.div`
   display: flex;
   justify-content: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  min-height: 300px;
-  max-width: 350px;
-  width: 100%;
-  padding: 30px;
-  background-color: rgb(141, 179, 157);
-  border-radius: 20px;
-  box-shadow: -15px -15px 2px -5px rgb(123 51 90 / 50%),
-    -15px 15px 2px -5px rgb(60 74 123 / 50%),
-    15px -15px 2px -5px rgb(255 0 0 / 50%),
-    15px 15px 2px -5px rgb(60 123 68 / 50%);
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+export const ModalContent = styled.div`
+  max-width: calc(100vw - 110px);
+  max-height: calc(100vh - 150px);
+  margin: 20px;
 `;
